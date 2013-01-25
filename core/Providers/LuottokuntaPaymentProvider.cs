e90c0a64280dacfb55fb3c9dc10025390b1467eb
kaa <jbeijar@mios.fi>
2013-01-25 10:07:28 +0200

    Use SHA256 as hash for luottokunta

--- core/Providers/LuottokuntaPaymentProvider.cs
+++ core/Providers/LuottokuntaPaymentProvider.cs
@@ -28,52 +28,56 @@ namespace Mios.Payment.Providers {
 			Url = parameters["url"] ?? Url;
 		}
 
+
 		public PaymentDetails GenerateDetails(string identifier, decimal amount, string returnUrl, string errorUrl, string message) {
 			var details = new PaymentDetails {
 				Url = Url,
 				Fields = new NameValueCollection(StringComparer.Ordinal) {
-					{"Merchant_Number",Account},
-					{"Card_Details_Transmit","0"},
-					{"Language","FI"},
-					{"Device_Category","1"},
-					{"Order_ID",identifier},
-					{"Amount",Math.Floor(amount*100).ToString(CultureInfo.InvariantCulture)},
-					{"Currency_Code","978"},
-					{"Order_Description",message},
-					{"Success_Url",returnUrl},
-					{"Failure_Url",errorUrl},
-					{"Cancel_Url",errorUrl},
-					{"Transaction_Type","1"}
-				}
+				{"Merchant_Number",Account},
+				{"Card_Details_Transmit","0"},
+				{"Language","FI"},
+				{"Device_Category","1"},
+				{"Order_ID",identifier},
+				{"Amount",Math.Floor(amount*100).ToString(CultureInfo.InvariantCulture)},
+				{"Currency_Code","978"},
+				{"Order_Description",message},
+				{"Success_Url",returnUrl},
+				{"Failure_Url",errorUrl},
+				{"Cancel_Url",errorUrl},
+				{"Transaction_Type","1"}
+			}
 			};
-			details.Fields["Authentication_Mac"] =
-				String.Format("{0}{1}{2}{3}{4}",
+			details.Fields["Authentication_Mac"] = 
+				String.Format("{0}&{1}&{2}&{3}&{4}&{5}",
 					details.Fields["Merchant_Number"],
 					details.Fields["Order_ID"],
 					details.Fields["Amount"],
+					details.Fields["Currency_Code"],
 					details.Fields["Transaction_Type"],
-					Secret).Hash("MD5").ToUpperInvariant();
+					Secret).Hash("SHA256").ToUpperInvariant();
 			return details;
 		}
 
 		public bool VerifyResponse(string identifier, decimal amount, NameValueCollection fields) {
 			var formattedAmount = Math.Floor(amount * 100).ToString(CultureInfo.InvariantCulture);
 			var expected =
-				String.Format("{0}{1}{2}{3}{4}",
+				String.Format("{0}&{1}&{2}&{3}&{4}&{5}",
 					Secret,
 					"1",
+					"978",
 					formattedAmount,
 					identifier,
-					Account).Hash("MD5").ToUpperInvariant();
+					Account).Hash("SHA256").ToLowerInvariant();
 			if(expected.Equals(fields["LKMAC"])) {
 				return true;
 			}
 			log.Error(
-				"Hash check failed when verifying response, expected {0} found {1}, value computed from {2}{3}{4}{5}{6}",
+				"Hash check failed when verifying response, expected {0} found {1}, value computed from {2}{3}{4}{5}{6}{7}",
 				expected,
 				fields["LKMAC"],
 				"SECRET",
 				"1",
+				"978",
 				formattedAmount,
 				identifier,
 				Account
