7e129d2323064a9d3f5a1c7d7b0db554a04ec96b
kaa <jbeijar@mios.fi>
2013-05-13 16:44:13 +0300

    Add test project.

---
+++ Tests/HMACSHATest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests {
+	public class HMACSHATest {
+		[Fact]
+		public void AgreesWithTestVectors() {
+			var key = "JefeJefeJefeJefeJefeJefeJefeJefe";
+			var msg = "what do ya want for nothing?";
+			var hmac = HMAC.Create("HMACSHA256");
+			hmac.Key = Encoding.UTF8.GetBytes(key);
+			var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(msg));
+			Assert.Equal("167f928588c5cc2eef8e3093caa0e87c9ff566a14794aa61648d81621a2a40c6", BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
+		}
+	}
+}
---
+++ Tests/ProcessPEMsTest.cs
@@ -0,0 +1,34 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.OpenSsl;
+using Xunit;
+using System.Security.Cryptography;
+
+namespace Tests {
+	public class ProcessPEMsTest {
+		[Fact]
+		public void ShouldReadRSAParametersFromPrivateKey() {
+			RSAParameters parameters;
+			using(var stream = typeof(ProcessPEMsTest).Assembly.GetManifestResourceStream("Tests.Resources.testkey.pem"))
+			using(var reader = new StreamReader(stream)) {
+				AsymmetricCipherKeyPair pair = null;
+				while(true) {
+					object obj = new PemReader(reader).ReadObject();
+					if(obj == null) break;
+					pair = obj as AsymmetricCipherKeyPair;
+				}
+				if(pair == null)
+					throw new ArgumentOutOfRangeException("The specified stream does not contain a private key");
+				parameters = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)pair.Private);
+			}
+			Assert.NotNull(parameters);
+		}
+	}
+}
