768fb133ede9e834c84ab171953c050cba55aff7
Joakim Beijar <jbeijar@mios.fi>
2014-01-13 11:13:01 +0200

    Add tests for Handelsbanken provider based on test vectors in documentation.

---
+++ Tests/Providers/HandelsbankenPaymentProviderTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mios.Payment.Providers;
+using Xunit;
+
+namespace Tests.Providers {
+	public class HandelsbankenPaymentProviderTests {
+		private static readonly NameValueCollection positiveTestVector 
+			= new NameValueCollection { 
+				{"butikid", "9999"},
+				{"ordernummer", "ABCD000001"},
+				{"orderbelopp", "1100"},
+				{"status", "0"},
+				{"timestamp", "20011015121000"},
+				{"kontrollsumma", "1f3caf897286c3159b65a705cf880570"},
+		};
+
+		private static readonly NameValueCollection negativeTestVector 
+			= new NameValueCollection(positiveTestVector) { 
+				{"status", "1"},
+				{"kontrollsumma", "b6914cca7702f96983a06c694b00ce34"},
+		};
+
+		[Fact]
+		public void GeneratedValuesShouldAgreeWithTestVectors() {
+			var now = new DateTimeOffset(new DateTime(2001, 10, 14, 12, 15, 00));
+			var expected = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase) {
+				{"entryid","switch"},
+				{"appaction","doDirectPay"},
+				{"switchaction","3"},
+				{"handOverDatatype","1"},
+				{"appname","ssse"},
+				{"language","sv"},
+				{"country","se"},
+				{"butikid","9999"},
+				{"ordernummer","ABCD000001"},
+				{"orderbelopp","1100"},
+				{"senastebokningstid", "20011015121500"},
+				{"retururl","https://www.butik.com/cgi/checkorder"},
+				{"kontrollsumma","26efb0517cdfbbacb13a61e91feae16d"}
+			};
+			var provider = new HandelsbankenPaymentProvider("account=9999&secret=aaaabbbb") {
+				Clock = () => now, Language = "sv", Country = "se"
+			};
+			var actual = provider.GenerateDetails(expected["ordernummer"], 1100m, expected["retururl"], expected["retururl"], "ignored");
+			Assert.Equal("https://secure.handelsbanken.se/bb/glss/servlet/ssco_dirapp", actual.Url);
+			foreach(var key in expected.Keys) {
+				Assert.Equal(expected[key], actual.Fields[key]);
+			}
+		}
+		[Fact]
+		public void ValidationShouldBeSuccessfulForPositiveTestVectors() {
+			var provider = new HandelsbankenPaymentProvider("account=9999&secret=aaaabbbb");
+			Assert.True(provider.VerifyResponse(positiveTestVector["ordernummer"], 1100m, positiveTestVector));
+		}
+		[Fact]
+		public void ValidationShouldFailForNegativeTestVectors() {
+			var provider = new HandelsbankenPaymentProvider("account=9999&secret=aaaabbbb");
+			Assert.False(provider.VerifyResponse(negativeTestVector["ordernummer"], 1100m, negativeTestVector));
+		}
+		[Fact]
+		public void ValidationShouldFailForMismatchedHash() {
+			var values = new NameValueCollection(positiveTestVector) {
+				{"kontrollsumma", "111111117286c3159b65a705cf880570"}
+			};
+			var provider = new HandelsbankenPaymentProvider("account=9999&secret=aaaabbbb");
+			Assert.False(provider.VerifyResponse(values["ordernummer"], 1100m, values));
+		}
+		[Fact]
+		public void ValidationShouldFailForMismatchedIdentifier() {
+			var provider = new HandelsbankenPaymentProvider("account=9999&secret=aaaabbbb");
+			Assert.False(provider.VerifyResponse("0000000001", 1100m, positiveTestVector));
+		}
+	}
+}
--- core/Providers/HandelsbankenPaymentProvider.cs
+++ core/Providers/HandelsbankenPaymentProvider.cs
@@ -9,12 +9,15 @@ namespace Mios.Payment.Providers {
 	public class HandelsbankenPaymentProvider : IPaymentProvider {
 		static readonly Logger log = LogManager.GetCurrentClassLogger();
 
+		public Func<DateTimeOffset> Clock { get; set; }
 		public string Account { get; set; }
 		public string Secret { get; set; }
 		public string Url { get; set; }
-		public string Currency { get; set; }
+		public string Country { get; set; }
+		public string Language { get; set; }
 		public HandelsbankenPaymentProvider() {
 			Url = "https://secure.handelsbanken.se/bb/glss/servlet/ssco_dirapp";
+			Clock = () => DateTimeOffset.Now;
 		}
 
 		public HandelsbankenPaymentProvider(string parameterString)
@@ -26,35 +29,37 @@ namespace Mios.Payment.Providers {
 			if(parameters["secret"] == null) {
 				throw new ArgumentException("Missing required 'secret' parameter in initialization string.");
 			}
+			Country = "se";
+			Language = "sv";
 			Account = parameters["account"];
 			Secret = parameters["secret"];
 			Url = parameters["url"] ?? Url;
 		}
 		public PaymentDetails GenerateDetails(string identifier, decimal amount, string returnUrl, string errorUrl, string message) {
 			var referenceNumber = ReferenceCalculator.GenerateReferenceNumber(identifier);
-			var formattedAmount = amount.ToString("N2", CultureInfo.CreateSpecificCulture("fi-fi"));
+			var formattedAmount = amount.ToString("F0", CultureInfo.CreateSpecificCulture("sv-se"));
 			var details = new PaymentDetails {
 				Url = Url,
-				Fields = new NameValueCollection(StringComparer.Ordinal) {
-					{ "entryid",  "switch"},
+				Fields = new NameValueCollection(StringComparer.OrdinalIgnoreCase) {
+					{ "entryid", "switch"},
 					{ "appaction", "doDirectPay"},
 					{ "switchaction", "3"},
 					{ "handOverDatatype", "1"},
 					{ "appname", "ssse"},
-					{ "language", "sv"},
-					{ "country",  "se"},
-					{ "butikid",     Account },
-					{ "ordernummer",    identifier },
-					{ "orderbelopp",    formattedAmount },
+					{ "language", Language},
+					{ "country", Country},
+					{ "butikid", Account },
+					{ "ordernummer", identifier },
+					{ "orderbelopp", formattedAmount },
 					{ "retururl", returnUrl },
-					{ "senastebokningstid", DateTime.Now.AddDays(1).ToString("yyyyMMddHHmmss") }
+					{ "senastebokningstid", Clock().AddDays(1).ToLocalTime().ToString("yyyyMMddHHmmss") }
 				}
 			};
-			details.Fields["Kontrollsumma"] =
-				String.Format("{0}{1}{2}{3}{4}{5}{6}{7}",
-					details.Fields["Butikid"],
-					details.Fields["Ordernummer"],
-					details.Fields["Orderbelopp"],
+			details.Fields["kontrollsumma"] =
+				String.Format("{0}{1}{2}{3}",
+					details.Fields["butikid"],
+					details.Fields["ordernummer"],
+					details.Fields["orderbelopp"],
 					Secret
 				).Hash("MD5").ToLowerInvariant();
 			return details;
@@ -67,12 +72,11 @@ namespace Mios.Payment.Providers {
 				return false;
 			}
 			if(!"0".Equals(fields["status"])) {
-				log.Error("Expected status {0} for successful payment, received {1}",
-					"0", fields["status"]);
+				log.Error("Expected status 0 for successful payment, received {0}", fields["status"]);
 				return false;
 			}
 			var expected =
-				String.Format("{0}{1}{2}{3}{4}{5}{6}",
+				String.Format("{0}{1}{2}{3}{4}{5}",
 					fields["butikid"],
 					fields["ordernummer"],
 					fields["orderbelopp"],
